e3ac7aab18f14e89b0e676fc49eb4e110376498f
Danik616 <ddanielvargas2011@gmail.com>
2022-10-31 19:26:12 -0500

    Se hace la creación del proyecto por SpringB

---
+++ draxter/src/main/java/com/draxter/draxter/DraxterApplication.java
@@ -0,0 +1,13 @@
+package com.draxter.draxter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraxterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraxterApplication.class, args);
+	}
+
+}
---
+++ draxter/src/test/java/com/draxter/draxter/DraxterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draxter.draxter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraxterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
