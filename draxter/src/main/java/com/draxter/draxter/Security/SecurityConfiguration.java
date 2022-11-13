71a9c47fcb3c52ea8ceb6fd0a9b940df16555cf9
Danik616 <ddanielvargas2011@gmail.com>
2022-11-13 01:11:23 -0500

    Se corrigen algunos metodos de security

--- draxter/src/main/java/com/draxter/draxter/Security/SecurityConfiguration.java
+++ draxter/src/main/java/com/draxter/draxter/Security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.draxter.draxter.Service.IUsuarioService;
 
@@ -51,7 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
         .permitAll()
         .and()
         .logout()
-        .invalideHttpSession(true)
+        .invalidateHttpSession(true)
         .clearAuthentication(true)
         .logoutRequestMatcher(new AntPathRequestMatcher("/salir"))
         .logoutSuccessUrl("/iniciar?salir")
--- draxter/src/main/java/com/draxter/draxter/Service/UsuarioService.java
+++ draxter/src/main/java/com/draxter/draxter/Service/UsuarioService.java
@@ -36,7 +36,6 @@ public class UsuarioService implements IUsuarioService {
     }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
         return null;
     }
 
