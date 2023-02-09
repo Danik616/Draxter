52522e1dfb35c519257305ccbbf33b81523fbd8e
Danik616 <ddanielvargas2011@gmail.com>
2023-02-09 16:41:27 -0500

    Pequeños cambios

--- draxter/src/main/java/com/draxter/draxter/Controller/DraxterInicioController.java
+++ draxter/src/main/java/com/draxter/draxter/Controller/DraxterInicioController.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+//import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+//import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.draxter.draxter.Entity.PQR;
 import com.draxter.draxter.Entity.Producto;
