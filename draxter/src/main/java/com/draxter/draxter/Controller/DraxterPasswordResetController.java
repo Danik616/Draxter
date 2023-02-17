245f7ccb13db0368b4490e47719d6f3d32d1fe7c
Danik616 <ddanielvargas2011@gmail.com>
2023-02-17 14:31:26 -0500

    Se agrega JQuery ademas, se hacen cambios al sistema

--- draxter/src/main/java/com/draxter/draxter/Controller/DraxterPasswordResetController.java
+++ draxter/src/main/java/com/draxter/draxter/Controller/DraxterPasswordResetController.java
@@ -1,17 +1,12 @@
 package com.draxter.draxter.Controller;
 
-import org.springframework.context.MessageSource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.draxter.draxter.Exception.InvalidTokenException;
@@ -20,6 +15,7 @@ import com.draxter.draxter.Service.CustomerAccountService;
 import com.draxter.draxter.data.ResetPasswordData;
 
 @Controller
+@RequestMapping("/password")
 public class DraxterPasswordResetController {
     private static final String REDIRECT_LOGIN = "redirect:/iniciarSesion";
     private static final String MSG = "resetPasswordMsg";
@@ -30,8 +26,8 @@ public class DraxterPasswordResetController {
     @Autowired
     private CustomerAccountService customerAccountService;
 
-    @PostMapping("/password/request")
-    public String resetPassword(@ModelAttribute("forgotPassword") ResetPasswordData forgotPasswordForm,
+    @PostMapping("/request")
+    public String resetPassword(@ModelAttribute("forgotPassword") final ResetPasswordData forgotPasswordForm,
             RedirectAttributes redirAttr) {
         try {
             customerAccountService.forgottenPassword(forgotPasswordForm.getEmail());
@@ -40,10 +36,10 @@ public class DraxterPasswordResetController {
         }
         redirAttr.addFlashAttribute(MSG,
                 messageSource.getMessage("user.forgotpwd.msg", null, LocaleContextHolder.getLocale()));
-        return REDIRECT_LOGIN;
+        return "redirect:/prueba";
     }
 
-    @GetMapping("/password/change")
+    @GetMapping("/change")
     public String changePassword(@RequestParam(required = false) String token, final RedirectAttributes redirAttr,
             final Model model) {
         if (StringUtils.isEmpty(token)) {
@@ -57,10 +53,10 @@ public class DraxterPasswordResetController {
         data.setToken(token);
         setResetPasswordForm(model, data);
 
-        return "/account/changePassword";
+        return "/changePassword";
     }
 
-    @PostMapping("/password/change")
+    @PostMapping("/change")
     public String changePassword(final ResetPasswordData data, final Model model) {
         try {
             customerAccountService.updatePassword(data.getPassword(), data.getToken());
@@ -70,12 +66,12 @@ public class DraxterPasswordResetController {
                     messageSource.getMessage("user.registration.verification.invalid.token", null,
                             LocaleContextHolder.getLocale()));
 
-            return "/account/changePassword";
+            return "/changePassword";
         }
         model.addAttribute("passwordUpdateMsg",
                 messageSource.getMessage("user.password.updated.msg", null, LocaleContextHolder.getLocale()));
         setResetPasswordForm(model, new ResetPasswordData());
-        return "/account/changePassword";
+        return "/changePassword";
     }
 
     private void setResetPasswordForm(final Model model, ResetPasswordData data) {
