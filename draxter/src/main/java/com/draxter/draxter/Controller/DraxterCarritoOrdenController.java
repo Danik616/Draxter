9b1d2bc35935d2db63c9e04d6475f1b981edf58c
Danik616 <ddanielvargas2011@gmail.com>
2023-02-25 18:04:57 -0500

    Se hacen cambios en la muestra del catalogo

--- draxter/src/main/java/com/draxter/draxter/Controller/DraxterCarritoOrdenController.java
+++ draxter/src/main/java/com/draxter/draxter/Controller/DraxterCarritoOrdenController.java
@@ -5,20 +5,14 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.draxter.draxter.Entity.Carrito;
 import com.draxter.draxter.Entity.Producto;
 import com.draxter.draxter.Entity.Usuarios;
 import com.draxter.draxter.Service.CarritoService;
