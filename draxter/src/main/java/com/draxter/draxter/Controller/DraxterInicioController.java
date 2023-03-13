82405f76cc902a5800d11e92d0093263c41b8fcd
Danik616 <ddanielvargas2011@gmail.com>
2023-03-13 15:47:25 -0500

    Se le hacen cambios a la plantilla de monitorear usuarios

--- draxter/src/main/java/com/draxter/draxter/Controller/DraxterInicioController.java
+++ draxter/src/main/java/com/draxter/draxter/Controller/DraxterInicioController.java
@@ -7,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
